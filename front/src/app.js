10600829708aec3ceaaf24e83e7d035a7499bf15
eunjin20107 <wjddmswls2017@gmail.com>
2023-02-12 21:36:42 +0900

    A

--- front/src/app.js
+++ front/src/app.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const path = require("path");
+
+const PORT = 4000; //나중에 바꿀 것
+const app = express();
+
+app.set("view engine", "pug");
+app.set(express.static(path.join(__dirname, "/assets")));
+app.use(morgan(`dev`));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} CaratTalk Server Start`);
+});
